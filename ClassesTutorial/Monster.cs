c448a8d256cc4acb2a42be7a62a0c876b7dc9bcb
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-02-28 18:09:31 +1030

    a

---
+++ ClassesTutorial/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    internal class Monster
+    {
+        public string name = "Monster";
+        public int health;
+        public int attackDamage;
+
+
+        public void attack()
+        {
+            Console.WriteLine($"{name} attacked for {attackDamage}");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            Console.WriteLine($"{name} was hit for {damage}");
+            Console.WriteLine($"{name} has {health} health left");
+        }
+    }
+}
--- ClassesTutorial/Program.cs
+++ ClassesTutorial/Program.cs
@@ -6,7 +6,14 @@ namespace ClassesTutorial // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Monster monsterOne = new Monster(); // Call the Monster class located in Monster.cs
+            monsterOne.health = 100; // Set the health the monster has
+            monsterOne.attackDamage = 2; // Set the amount of damage the monster deals
+
+            monsterOne.attack(); // Make the monster attack
+            monsterOne.TakeDamage(20); // Make the monster take 20 damage
+
+            Console.ReadKey();
         }
     }
 }
